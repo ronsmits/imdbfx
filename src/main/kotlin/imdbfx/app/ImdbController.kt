2c859e392a6bc3b72b78f3f249929614db759b58
Ron Smits <ron.smits@gmail.com>
2017-11-02 15:40:30 +0100

    first attempt at communicating with the rest endpoint

---
+++ src/main/kotlin/imdbfx/app/ImdbController.kt
@@ -0,0 +1,18 @@
+package imdbfx.app
+
+import tornadofx.*
+
+class ImdbController: Controller() {
+    val api : Rest by inject()
+    init {
+        api.baseURI = "http://theimdbapi.org/api/find/"
+    }
+    fun getMovie(title: String, year : Int) {
+        val params = mutableMapOf("title" to title)
+        if(year > 1800) params.put("year", year.toString())
+        println(params.queryString)
+        println(api.getURI("movie${params.queryString}"))
+        val resp : Rest.Response? = try {api.get("movie${params.queryString}")} catch (ex: RestException) { null}
+        println("rest is set to $resp")
+    }
+}
--- src/main/kotlin/imdbfx/model/Movie.kt
+++ src/main/kotlin/imdbfx/model/Movie.kt
@@ -1,19 +1,26 @@
 package imdbfx.model
 
+import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 import javax.json.JsonObject
 import tornadofx.getValue
 import tornadofx.setValue
+import java.net.URL
 
 class Movie : JsonModel {
 
     val titleProperty = SimpleStringProperty()
     var title by titleProperty
+    val thumbnailProperty = SimpleObjectProperty<URL>()
+    var thumbnail by thumbnailProperty
 
     override fun updateModel(json: JsonObject) {
         with (json){
             title = string("title")
+           val imageholder = getJsonObject("poster")?: getJsonObject("image")
+            println(imageholder)
+            thumbnail = URL(imageholder.getString("thumb"))
         }
     }
 }
